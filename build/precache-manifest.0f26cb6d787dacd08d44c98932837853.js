adb3c6e2adb18f87f1456d49fe5419b434be869e
Aidan Sweeny <apswee22@colby.edu>
2020-08-19 23:38:08 -0400

    Added dependencies

---
+++ build/precache-manifest.0f26cb6d787dacd08d44c98932837853.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f54c68f0fb503329111b",
+    "url": "/react-portfolio/static/js/main.2f0ae896.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "00303447530f3cde10e8",
+    "url": "/react-portfolio/static/css/2.d1cc8024.chunk.css"
+  },
+  {
+    "revision": "00303447530f3cde10e8",
+    "url": "/react-portfolio/static/js/2.537f841c.chunk.js"
+  },
+  {
+    "revision": "d79a52917ab7dcb244f2f727f72386a3",
+    "url": "/react-portfolio/static/media/financhill.d79a5291.jpg"
+  },
+  {
+    "revision": "46fa894becf60c3b2cd290b5e6ddc712",
+    "url": "/react-portfolio/static/media/clicky-game.46fa894b.jpg"
+  },
+  {
+    "revision": "090825ec549460bae6f4c2f385a6ae5f",
+    "url": "/react-portfolio/static/media/express-dogs.090825ec.jpg"
+  },
+  {
+    "revision": "c1faca1ca420f3e5e1c67c9737d9d4db",
+    "url": "/react-portfolio/static/media/life.c1faca1c.jpg"
+  },
+  {
+    "revision": "1ac7c113a1b4636936517fbc9eb89270",
+    "url": "/react-portfolio/static/media/note-taker.1ac7c113.jpg"
+  },
+  {
+    "revision": "0beac7bcd144c06c58fca5a77d4a6f3b",
+    "url": "/react-portfolio/static/media/weather.0beac7bc.jpg"
+  },
+  {
+    "revision": "da0c149f896b22c291ef9656a0ae88b8",
+    "url": "/react-portfolio/index.html"
+  }
+];
