0c361a63ef00c27412b3e9f4300003b4235ee80b
Aidan Sweeny <apswee22@colby.edu>
2020-08-20 01:16:13 -0400

    added updated resume

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import resume from "./Aidan-Sweeny-Resume.pdf";
 
 class Footer extends React.Component {
     render() {
@@ -11,7 +11,7 @@ class Footer extends React.Component {
                 <h3 style = {{zIndex : "2", position : "fixed", bottom : "105px",right : "10px"}} >Contact Information</h3>
                 <a style = {{zIndex : "2", position : "fixed", bottom : "85px",right : "10px"}} href="https://github.com/AidanSweeny">GitHub Profile</a>
                 <a style = {{zIndex : "2", position : "fixed", bottom : "70px",right : "10px"}} href="https://www.linkedin.com/in/aidan-sweeny-81075030/">LinkedIn</a>
-                <a style = {{zIndex : "2", position : "fixed", bottom : "55px",right : "10px"}} href="../images/Aidan-Sweeny-Resume.pdf">Resume</a>
+                <a style = {{zIndex : "2", position : "fixed", bottom : "55px",right : "10px"}} href={resume}>Resume</a>
                 <p style = {{zIndex : "2", position : "fixed", bottom : "20px",right : "10px"}} >Phone: (415)-300-7568</p>
                 <p style = {{zIndex : "2", position : "fixed", bottom : "0px",right : "10px"}} >Email: aidansweeny@gmail.com</p>
             </div>
